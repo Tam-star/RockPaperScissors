f467924b433d0e73ed807bdd34b0e9e20ad8eb0e
Tamara Wenner <tamara-76@hotmail.fr>
2022-02-11 15:59:13 +0100

    Started responsive

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import Header from "./components/Header/Header";
 import ChoiceContainer from "./components/ChoiceContainer/ChoiceContainer";
 import RulesButton from "./components/RulesButton/RulesButton";
--- src/components/RulesModal/RulesModal.js
+++ src/components/RulesModal/RulesModal.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./RulesModal.scss";
 
-function RulesModal({closeModal}) {
+function RulesModal({ closeModal }) {
   return (
     <>
       <section className="rules-modal" data-testid="rules-modal">
         <h1>RULES</h1>
-        <img alt="" className="close-icon" src="/images/icon-close.svg" onClick={closeModal}></img>
         <img alt="" className="rules-img" src="/images/image-rules.svg"></img>
+        <img
+          alt=""
+          className="close-icon"
+          src="/images/icon-close.svg"
+          onClick={closeModal}
+        ></img>
       </section>
       <div className="shadow"></div>
     </>
